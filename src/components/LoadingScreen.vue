defbddb23eb1b64c40bc07f2dbe34763f0011321
Eugen Sawitzki <eugensawitzki@gmail.com>
2018-03-28 21:28:03 +0200

    working on loadingscreen and logic

---
+++ src/components/LoadingScreen.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="spinner-background">
+    <pulse-spinner></pulse-spinner>
+    <br>
+    <h2>Loading...</h2>
+  </div>
+</template>
+
+<script>
+  import PulseSpinner from 'vue-spinner/src/PulseLoader.vue'
+  
+  export default {
+    components: {
+      PulseSpinner
+    }
+  }
+</script>
+
+<style>
+  .spinner-background {
+    height: 100%;
+    width: 100%;
+    background-color: rgba(0,0,0,0.7);
+    padding-top: 50px;
+    justify-content: center;
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 10000;
+  }
+
+</style>
